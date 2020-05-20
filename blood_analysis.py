12e57fea1ea5b63d7442ec83a8ec5ae41b290fd5
zjk41 <jz314@duke.edu>
2020-05-20 10:53:44 -0400

    Analyzes and reports HDL status

--- blood_analysis.py
+++ blood_analysis.py
@@ -1,7 +1,23 @@
 # blood_analysis.py
 
+def HDL_analysis(HDL_result):
+    if HDL_result >= 60:
+        return "Good"
+    elif 40 <= HDL_result < 60:
+        return "Borderline"
+    else: 
+        return "Bad"
+        
+
 def HDL_interface():
+    # Input should be HDL=66
     print("HDL_interface")
+    print("Please input the result in the following format:")
+    print("  HDL=## where ## is the numerical result")
+    HDL_input = input("Result: ")
+    HDL_result = HDL_input.split('=')
+    HDL_status = HDL_analysis(int(HDL_result[1]))
+    print("HDL_status is {}".format(HDL_status))
     pass
     
     
